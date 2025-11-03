6f251f2ac071ce357f7a1c5b5257ba65c7f9b7d8
SherazDeveloper12 <talhayounus065@gmail.com>
2025-11-03 20:02:42 +0500

    Add MongoDB connection and product creation route; update dependencies

--- server.js
+++ server.js
@@ -1,6 +1,57 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// mongodb connection setup can be added here
+const mongoose = require('mongoose');
+const Cat = mongoose.model('Cat', { name: String });
+// Middleware setup
+
+app.use(bodyParser.json());
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// Sample route to test the server
+// MongoDB connection
+
+  async function main() {
+    try {
+        await mongoose.connect('mongodb+srv://sherazLearning:Ytguru@65@cluster0.umlgkew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        console.log('Connected to MongoDB successfully');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // Exit the process if connection fails
+        
+    }
+  
+}
+
+main()
+// define sceheme for products
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+// sample routre to create a product
+app.post('/products/create', async (req, res) => {
+    const newProduct = new Product({
+        name: req.body.name,
+        price: req.body.price
+    });
+
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(201).json(savedProduct);
+    } catch (err) {
+        res.status(400).json({ message: err.message 
+
+        });
+    }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
