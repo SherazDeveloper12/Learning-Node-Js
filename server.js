a080d580ad2c971614d9c4964885f1d96a5942b3
SherazDeveloper12 <talhayounus065@gmail.com>
2025-11-04 21:20:01 +0500

    Implement user authentication and CRUD operations; add bcrypt and jsonwebtoken dependencies

--- server.js
+++ server.js
@@ -4,12 +4,31 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+const  bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const authverify = (req, res, next) => {
+   try {
+    if (!req.headers.authorization) {
+        return res.status(401).json({ message: 'Login Required' });
+    }
+    const decoded = jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
+    if (!decoded) {
+        return res.status(401).json({ message: 'Login Required' });
+    }
+    console.log('Decoded JWT:', decoded);
+    req.body.user = decoded;
+    next();
+    
+   } catch (error) {
+    res.status(401).json({ message: error.message });
+   }
+};
 // MongoDB Connection
 async function main() {
     try {
@@ -37,7 +56,19 @@ const productSchema = new mongoose.Schema({
     category: String,
 });
 const Product = mongoose.model('Product', productSchema);
+// user schema can be added here similarly
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true
+    },
+    email: { type: String, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true },
+    address: String,
+    createdAt: { type: Date, default: Date.now }
 
+});
+const User = mongoose.model('User', userSchema);
 
 // Routes
 app.get('/', (req, res) => {
@@ -56,7 +87,7 @@ app.post('/products/create', async (req, res) => {
     }
 });
 
-app.get('/products', async (req, res) => {
+app.get('/products', authverify, async (req, res) => {
     try {
         const products = await Product.find();
         res.json(products);
@@ -100,8 +131,87 @@ app.get('/products/:id', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+// ==================== Users CRUD ROUTES ====================
+// User routes can be added here similarly
+app.post('/users/signup', async (req, res) => {
+    try {
+        if (!req.body.password) {
+            return res.status(400).json({ message: 'Password is required' });
+        }
+        var hashedPassword =  bcrypt.hashSync(req.body.password, 10);
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+            address: req.body.address
+        });
+        const saved = await newUser.save();
+        console.log('User created:', saved);
+        res.status(201).json(saved);
+    } catch (err) {
+        res.status(402).json({ message: err.message });
+    }
+});
 
+app.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const isMatch = bcrypt.compareSync(req.body.password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        var token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
+        const loggedInUser = {
+            token: token,
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                address: user.address,
+                createdAt: user.createdAt
+            }
+        res.json({ message: 'Login successful', loggedInUser });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.put('/users/update/:id', async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                username: req.body.username,
+                email: req.body.email,
+                address: req.body.address,
+                password: req.body.password ? bcrypt.hashSync(req.body.password, 10) : undefined
+            },
+            { new: true }
+        );
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+app.delete('/users/delete/:id', async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        res.json(deletedUser);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
